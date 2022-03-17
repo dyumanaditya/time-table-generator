08fb201111d6df06de2adc3311c4cbb1301e1392
AmitVankayala <96190218+AmitVankayala@users.noreply.github.com>
2022-03-17 10:36:24 +0530

    editing OCR text

--- python_random_data_generator/students.py
+++ python_random_data_generator/students.py
@@ -8,11 +8,33 @@ Arabinda Sahoo
 Ashpriha Biswas 
 Ashwini
 Asmita Das
-Aurobrata Panda 
+Aurobrata Panda
+Bhaswar Ghosh
+Dyuman Aditya (A.G.)
+Harshini Nilakantan
+Jenish Halvadia
+Jeyasuriya Gopinath
+Meera Joshi 18 Narendran (K) 19 Praketa Kumar Behera 20 Ramyak Samantaray 21 Ritam Lenka 22 Sayuri Mima 17 Kruti Shah 18 Raja Banerjee 19 Sarthak Bhattacharya 20 Selvamira (D) 21 Shivani (M) 22 Subhasini Mohanty 23 Sephali Priyadarshini Nay, 23 Suchitra Rani Sahu 25 Smruti Ranjan Sahoo 25 Suvin Varma 26 Swapna Sahoo 27 Tenzing N Rigdrol- 28 Tenzing N Rigzang 29
 Surajit Acharya
 
+Aaryan
+Abeni Manzar
+Aditi Mandali
+Aditi Sharma
+Aditya Kumar Mahapatra
+Ahana Ghosh
+Aniket Deb
+Anushka Mitra
+Aurokrishna (S)
+Devdatta Chatterjee
+Harshita Nilakantan
+Janaki Pande
+Kirttiparnna Swain
+Kush Patel
+Markendeya Achille Morsiani
 Shubha Sanket Samantaray
 Sreevidhya
+Swarnojit Bhattacharjee 
 
 Aditya Patra
 Aishi Dey
@@ -40,4 +62,9 @@ Shashwati Dey
 Shinjini Roy
 Skandhan Karthikeyan
 Smita Chaturvedy 
-Souryashree Samarpan 26  27 Sumit Singh 28 Sunanda Sahoo 29 Urva Chandrani 30 Vaiditi Vipul Acharya 31 Vivek-Alex Sharan 1|Aaryan 2 Abeni Manzar 3 Aditi Mandali 4 Aditi Sharma SlAditya Kumar Mahapatra 6 Ahana Ghosh 7Aniket Deb 8 Anushka Mitra 9|Aurokrishna (S) 10 Devdatta Chatterjee 11Harshita Nilakantan 12 Janaki Pande 12 Bhaswar Ghosh 13 Dyuman Aditya (A.G.) 14 Harshini Nilakantan 15 Jenish Halvadia 14 Kirttiparnna Swain 15 Kush Patel 16 Markendeya Achille Mors 16 Jeyasuriya Gopinath 17 Meera Joshi 18 Narendran (K) 19 Praketa Kumar Behera 20 Ramyak Samantaray 21 Ritam Lenka 22 Sayuri Mima 17 Kruti Shah 18 Raja Banerjee 19 Sarthak Bhattacharya 20 Selvamira (D) 21 Shivani (M) 22 Subhasini Mohanty 23 Sephali Priyadarshini Nay, 23 Suchitra Rani Sahu 25 Smruti Ranjan Sahoo 25 Suvin Varma 26 Swapna Sahoo 27 Tenzing N Rigdrol- 28 Tenzing N Rigzang 29 Swarnojit Bhattacharjee 29 Uttama (T) 30 Vankayala Amit 27 Sumitaa Ghosal 28 Svaha Myvannan 30 Vibhu Dalal
+Souryashree Samarpan
+Sumit Singh
+Sunanda Sahoo
+Urva Chandrani
+Vaiditi Vipul Acharya
+Vivek-Alex Sharan 16 17 29 Uttama (T) 30 Vankayala Amit 27 Sumitaa Ghosal 28 Svaha Myvannan 30 Vibhu Dalal
