b2177f01ffde19c175f42b0bdafae18f215e7f26
AmitVankayala <96190218+AmitVankayala@users.noreply.github.com>
2022-03-17 10:28:14 +0530

    editing the OCR text

---
+++ python_random_data_generator/students.py
@@ -0,0 +1,43 @@
+Aadhini Kannappan
+Adithya Senthilnathan 
+Akanksha Galagali 
+Aniruddha Paik 
+Ankita Sahoo 
+Anoushka Rai 
+Arabinda Sahoo
+Ashpriha Biswas 
+Ashwini
+Asmita Das
+Aurobrata Panda 
+Surajit Acharya
+
+Shubha Sanket Samantaray
+Sreevidhya
+
+Aditya Patra
+Aishi Dey
+Ananyaa Baskar
+Arijit Podder
+Arundhati Panda
+Asmi Dolia
+Auro Adarsh
+Baivab Dutta
+Chaitanya Sharma
+Daffodil Mazumdar
+Divyanshu Pradhan
+Kaninika Majumdar
+Lakshman Baliyarsingh
+Jishnu (P.S.)
+Naman R Singh 
+Pratyush Jena
+Rayih Chatterjee
+Rishabh Mukund
+Ritam Mondal
+Sai Roshini Miriyala
+Samarpan Jena
+Saurabh Ranjan Das
+Shashwati Dey
+Shinjini Roy
+Skandhan Karthikeyan
+Smita Chaturvedy 
+Souryashree Samarpan 26  27 Sumit Singh 28 Sunanda Sahoo 29 Urva Chandrani 30 Vaiditi Vipul Acharya 31 Vivek-Alex Sharan 1|Aaryan 2 Abeni Manzar 3 Aditi Mandali 4 Aditi Sharma SlAditya Kumar Mahapatra 6 Ahana Ghosh 7Aniket Deb 8 Anushka Mitra 9|Aurokrishna (S) 10 Devdatta Chatterjee 11Harshita Nilakantan 12 Janaki Pande 12 Bhaswar Ghosh 13 Dyuman Aditya (A.G.) 14 Harshini Nilakantan 15 Jenish Halvadia 14 Kirttiparnna Swain 15 Kush Patel 16 Markendeya Achille Mors 16 Jeyasuriya Gopinath 17 Meera Joshi 18 Narendran (K) 19 Praketa Kumar Behera 20 Ramyak Samantaray 21 Ritam Lenka 22 Sayuri Mima 17 Kruti Shah 18 Raja Banerjee 19 Sarthak Bhattacharya 20 Selvamira (D) 21 Shivani (M) 22 Subhasini Mohanty 23 Sephali Priyadarshini Nay, 23 Suchitra Rani Sahu 25 Smruti Ranjan Sahoo 25 Suvin Varma 26 Swapna Sahoo 27 Tenzing N Rigdrol- 28 Tenzing N Rigzang 29 Swarnojit Bhattacharjee 29 Uttama (T) 30 Vankayala Amit 27 Sumitaa Ghosal 28 Svaha Myvannan 30 Vibhu Dalal
