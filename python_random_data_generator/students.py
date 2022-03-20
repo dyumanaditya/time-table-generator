2cc1129696f4314005e8aa8e9568c00408e129e6
mal-micro <mal-micro-rock@protonmail.ch>
2022-03-20 22:58:10 +0530

    OCR sorting of students done

--- python_random_data_generator/students.py
+++ python_random_data_generator/students.py
@@ -1,70 +1,102 @@
-Aadhini Kannappan
-Adithya Senthilnathan 
-Akanksha Galagali 
-Aniruddha Paik 
-Ankita Sahoo 
-Anoushka Rai 
-Arabinda Sahoo
-Ashpriha Biswas 
-Ashwini
-Asmita Das
-Aurobrata Panda
-Bhaswar Ghosh
-Dyuman Aditya (A.G.)
-Harshini Nilakantan
-Jenish Halvadia
-Jeyasuriya Gopinath
-Meera Joshi 18 Narendran (K) 19 Praketa Kumar Behera 20 Ramyak Samantaray 21 Ritam Lenka 22 Sayuri Mima 17 Kruti Shah 18 Raja Banerjee 19 Sarthak Bhattacharya 20 Selvamira (D) 21 Shivani (M) 22 Subhasini Mohanty 23 Sephali Priyadarshini Nay, 23 Suchitra Rani Sahu 25 Smruti Ranjan Sahoo 25 Suvin Varma 26 Swapna Sahoo 27 Tenzing N Rigdrol- 28 Tenzing N Rigzang 29
-Surajit Acharya
+# K3 students
+k3_students = [
+"Aadhini Kannappan",
+"Adithya Senthilnathan",
+"Akanksha Galagali",
+"Aniruddha Paik",
+"Ankita Sahoo",
+"Anoushka Rai",
+"Arabinda Sahoo",
+"Ashpriha Biswas",
+"Ashwini",
+"Asmita Das",
+"Aurobrata Panda",
+"Bhaswar Ghosh",
+"Dyuman Aditya (A.G.)",
+"Harshini Nilakantan",
+"Jenish Halvadia",
+"Jeyasuriya Gopinath",
+"Kruti Shah",
+"Raja Banerjee",
+"Sarthak Bhattacharya",
+"Selvamira (D)",
+"Shivani (M)",
+"Subhasini Mohanty",
+"Suchitra Rani Sahu",
+"Suvin Varma",
+"Swapna Sahoo",
+"Surajit Acharya",
+"Tenzing N Rigdrol",
+"Tenzing N Rigzang",
+"Uttama (T)",
+"Vankayala Amit"
+]
 
-Aaryan
-Abeni Manzar
-Aditi Mandali
-Aditi Sharma
-Aditya Kumar Mahapatra
-Ahana Ghosh
-Aniket Deb
-Anushka Mitra
-Aurokrishna (S)
-Devdatta Chatterjee
-Harshita Nilakantan
-Janaki Pande
-Kirttiparnna Swain
-Kush Patel
-Markendeya Achille Morsiani
-Shubha Sanket Samantaray
-Sreevidhya
-Swarnojit Bhattacharjee 
+# K2 students
+k2_students = [
+"Aaryan Ananda",
+"Abeni Manzar",
+"Aditi Mandali",
+"Aditi Sharma",
+"Aditya Kumar Mahapatra",
+"Ahana Ghosh",
+"Aniket Deb",
+"Anushka Mitra",
+"Aurokrishna (S)",
+"Devdatta Chatterjee",
+"Harshita Nilakantan",
+"Janaki Pande",
+"Kirttiparnna Swain",
+"Kush Patel",
+"Markendeya Achille Morsiani",
+"Meera Joshi",
+"Narendran (K)",
+"Praketa Kumar Behera",
+"Ramyak Samantaray",
+"Ritam Lenka",
+"Sayuri Mima",
+"Sephali Priyadarshini Nay",
+"Shubha Sanket Samantaray",
+"Smruti Ranjan Sahoo",
+"Sreevidhya",
+"Sumitaa Ghosal",
+"Svaha Myvannan",
+"Swarnojit Bhattacharjee",
+"Vibhu Dalal"
+]
 
-Aditya Patra
-Aishi Dey
-Ananyaa Baskar
-Arijit Podder
-Arundhati Panda
-Asmi Dolia
-Auro Adarsh
-Baivab Dutta
-Chaitanya Sharma
-Daffodil Mazumdar
-Divyanshu Pradhan
-Kaninika Majumdar
-Lakshman Baliyarsingh
-Jishnu (P.S.)
-Naman R Singh 
-Pratyush Jena
-Rayih Chatterjee
-Rishabh Mukund
-Ritam Mondal
-Sai Roshini Miriyala
-Samarpan Jena
-Saurabh Ranjan Das
-Shashwati Dey
-Shinjini Roy
-Skandhan Karthikeyan
-Smita Chaturvedy 
-Souryashree Samarpan
-Sumit Singh
-Sunanda Sahoo
-Urva Chandrani
-Vaiditi Vipul Acharya
-Vivek-Alex Sharan 16 17 29 Uttama (T) 30 Vankayala Amit 27 Sumitaa Ghosal 28 Svaha Myvannan 30 Vibhu Dalal
+# K1 students
+k1_students = [
+"Aditya Patra",
+"Aishi Dey",
+"Ananyaa Baskar",
+"Arijit Podder",
+"Arundhati Panda",
+"Asmi Dolia",
+"Auro Adarsh",
+"Baivab Dutta",
+"Chaitanya Sharma",
+"Daffodil Mazumdar",
+"Divyanshu Pradhan",
+"Kaninika Majumdar",
+"Lakshman Baliyarsingh",
+"Jishnu (P.S.)",
+"Naman R Singh",
+"Pratyush Jena",
+"Rayih Chatterjee",
+"Rishabh Mukund",
+"Ritam Mondal",
+"Sai Roshini Miriyala",
+"Samarpan Jena",
+"Saurabh Ranjan Das",
+"Shashwati Dey",
+"Shinjini Roy",
+"Skandhan Karthikeyan",
+"Smita Chaturvedy",
+"Souryashree Samarpan",
+"Sumit Singh",
+"Sunanda Sahoo",
+"Urva Chandrani",
+"Vaiditi Vipul Acharya",
+"Vivek-Alex Sharan"
+]
