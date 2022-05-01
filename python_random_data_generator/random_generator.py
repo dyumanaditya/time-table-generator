f6aa5fc058b7e0b7f875de979aa271925195b9df
Mal Micro <mal-micro-rock@protonmail.ch>
2022-05-02 01:15:47 +0530

    DONE WITH THE RANDOM STUDENT DATA GENERATOR!

--- python_random_data_generator/random_generator.py
+++ python_random_data_generator/random_generator.py
@@ -1,8 +1,9 @@
 import csv
 import random
 import string
+from types import NoneType
 
-# my imports
+# My imports
 import students
 import courses
 import parameters
@@ -15,7 +16,7 @@ class RandomGenerator:
     def generate_teacher_csv(self):
         pass
 
-    def generate_student_csv(self, section:string):
+    def generate_student_csv(self, section: string) -> NoneType:
         if section == "K1": self.students = students.k1_students
         elif section == "K2": self.students = students.k2_students
         else: self.students = students.k3_students
@@ -30,93 +31,78 @@ class RandomGenerator:
 
             writer.writerow(column_titles)
             # -----------------
-
+            # Generating data for each student in a list
             for student in self.students:
-
-                # adding the first number (1-9) so that 0 is not the first number,
-                #  then all the ten numbers will appear when you preview the CSV file
-                phone = str(random.randint(1,9))
+                phone = self.__generate_phone()
                 number_of_courses = random.randint(parameters.min_courses, parameters.max_courses)
                 remaining_periods = parameters.max_periods
-
-                for i in range(0,9):
-                    phone += str(random.randint(0, 9))
-
-                student_data = [
-                        student, section, student.split()[0].lower() + ".email@poop.com", phone, number_of_courses
-                    ]
+                student_data = [ student, section, student.split()[0].lower() + ".email@poop.com", phone, number_of_courses ]
 
                 for i in range(number_of_courses + 1):
                     if not remaining_periods: break
                     # Selecting a random course from the list
                     course = courses.courses[random.randint(0, len(courses.courses)-1)]
+                    course = self.__check_if_section_specific_course(course, section)                            
 
-                    # Adding the section name as a suffix to some specific courses
-                    if course in courses.section_specific_course:
-                        for c in range(0, len(courses.section_specific_course)):
-                            pass
-                        course = course + "_" + section.lower()                            
-
-                    for c in range(0, len(courses.courses_fixed_periods)):
-                        if course.split("_")[0] in courses.courses_fixed_periods:
-                            course_tuple = self.check_remaining_periods(remaining_periods, courses.courses_fixed_periods_with_periods[course.split("_")[0]], course)
-                            course = course_tuple[0]
-                            print(type(course_tuple[1]), course_tuple[1])
-                            remaining_periods = int(course_tuple[1])
-                            # print(course_tuple[1])
-                        else:
-                            number_of_periods = random.randint(parameters.min_periods_per_subject, parameters.max_periods_per_subject)
-                            course_tuple = self.check_remaining_periods(remaining_periods, number_of_periods, course)
-                            course = course_tuple[0]
-                            # remaining_periods -= int(course.split(";")[1])
-                            print(type(course_tuple[1]), course_tuple[1])
-                            remaining_periods = int(course_tuple[1])
-                            # print(course_tuple[1])
+                    number_of_periods = 0
+                    if course.split("_")[0] in courses.courses_fixed_periods:
+                        number_of_periods = courses.courses_fixed_periods_with_periods[course.split("_")[0]]
+                    else:
+                        number_of_periods = random.randint(parameters.min_periods_per_subject, parameters.max_periods_per_subject)
+                    
+                    course_tuple = self.check_remaining_periods(remaining_periods, number_of_periods, course)
+                    course = course_tuple[0]
+                    remaining_periods = int(course_tuple[1])
                     # --------------------
                     
                     student_data.append(course)
                 
-                # writer.writerow(student_data)
+                writer.writerow(student_data)
     
-    def check_remaining_periods(self, remaining_periods, number_of_periods, course):
-        # print(type(remaining_periods), remaining_periods)
+    def check_remaining_periods(self, remaining_periods: int, number_of_periods: int, course: string) -> tuple:
         if number_of_periods > remaining_periods:
             return self.select_course(remaining_periods, number_of_periods, course)
         else:
-            return course + ";" + str(number_of_periods)
+            return (self.__add_number_of_periods_after_colon(course, number_of_periods), remaining_periods - number_of_periods)
 
-    def select_course(self, remaining_periods, number_of_periods, course):
+    def select_course(self, remaining_periods: int, number_of_periods: int, course: string) -> tuple:
         if number_of_periods > remaining_periods:
-            course = courses.courses[random.randint(0, len(courses.courses)-1)]
-            
             if course in courses.courses_fixed_periods:
-                number_of_periods = courses.courses_fixed_periods_with_periods[course]
                 while (number_of_periods > remaining_periods):
                     course = courses.courses[random.randint(0, len(courses.courses)-1)]
-                    if course in courses.courses_fixed_periods: 
-                        if remaining_periods > courses.courses_fixed_periods_with_periods[course]:
-                            remaining_periods -= courses.courses_fixed_periods_with_periods[course]
-                            print(remaining_periods)
-                            return (course, remaining_periods)
+                    if course in courses.courses_fixed_periods:
+                        number_of_periods = courses.courses_fixed_periods_with_periods[course]
+                        if remaining_periods >= number_of_periods:
+                            remaining_periods -= number_of_periods
+                            return (self.__add_number_of_periods_after_colon(self.__check_if_section_specific_course(course), number_of_periods), remaining_periods)
                     else:
-                        number_of_periods = random.randint(parameters.min_periods_per_subject, parameters.max_periods_per_subject)
-                        if number_of_periods <= number_of_periods: 
-                            remaining_periods -= number_of_periods
-                            print(remaining_periods)
-                            return (course, remaining_periods)
-                        elif remaining_periods > 0: 
-                            remaining_periods -= number_of_periods
-                            print(remaining_periods)
-                            return (course, remaining_periods)
-
-            remaining_periods -= number_of_periods
-            print(remaining_periods)
-            return (course, remaining_periods)
-
+                        while (number_of_periods > remaining_periods):
+                            number_of_periods = random.randint(parameters.min_periods_per_subject, parameters.max_periods_per_subject)
+                        
+                        remaining_periods -= number_of_periods
+                        return (self.__add_number_of_periods_after_colon(course, number_of_periods), remaining_periods)
+            else:
+                remaining_periods -= number_of_periods
+                return (self.__add_number_of_periods_after_colon(course, number_of_periods), remaining_periods)
         else:
             remaining_periods -= number_of_periods
-            print(remaining_periods)
             return (course, remaining_periods)
+
+    def __generate_phone(self) -> str:
+        # Adding the first number (1-9) so that 0 is NOT the first number,
+        #  so that the number appears to be 10 digit, and NOT 9 digit.
+        _phone = str(random.randint(1,9))
+        for i in range(0,9):
+            _phone += str(random.randint(0, 9))
+        return _phone
+
+    def __check_if_section_specific_course(self, _course: str, section: str) -> str:
+        # Adding the section name as a suffix to some specific courses
+        if _course in courses.section_specific_course: return _course + "_" + section.lower()
+        else: return _course
+
+    def __add_number_of_periods_after_colon(self, _course: str, _number_of_periods: int) -> str:
+        return _course + ";" + str(_number_of_periods)
 
 
 random_generator = RandomGenerator()
